35f8b8d24a518a600f97f8afab7eac603eea9035
Dmitry <44382546+zeeeeby@users.noreply.github.com>
2022-10-09 20:15:21 +0300

    Time: 111 ms (41.28%), Space: 44.5 MB (51.96%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swap(head: ListNode | null) {
+        if (!head || !head.next) return head;
+     
+        let newHead = head.next;
+        head.next = head.next.next;
+        newHead.next = head;
+    
+        head.next = swap(head.next);
+        return newHead;
+}
+function swapPairs(head: ListNode | null): ListNode | null {
+    return swap(head);
+};
