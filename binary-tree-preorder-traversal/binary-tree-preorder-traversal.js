7e60aa10b3bc47c1b54b7280699d56803f7bf148
Dmitry <44382546+zeeeeby@users.noreply.github.com>
2022-10-16 18:01:18 +0300

    Time: 95 ms, Memory: 42.2 MB - LeetHub

---
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+const traverse = (root, ans) => {
+    if (root) {
+        ans.push(root.val);
+        traverse(root.left, ans);
+        traverse(root.right, ans);
+    }
+}
+const traverseNonRecursive = (root, cb) => {
+    if (root) {
+        const stack = [root.right, root.left, root.val] 
+        while (stack.length) {
+            const curr = stack.pop()
+            if (curr === null || curr === undefined) continue
+            if (!(curr instanceof TreeNode)) {
+                cb(curr)
+            }
+            else {
+                stack.push(curr.right, curr.left, curr.val)
+            }
+        }
+    }
+}
+var preorderTraversal = function(root) {
+    const ans = [];
+    // traverse(root, ans);
+    traverseNonRecursive(root, x => ans.push(x));
+    return ans;
+};
