45815bca421327922ab7b2437c11cb376f29c46b
Dmitry <44382546+zeeeeby@users.noreply.github.com>
2022-10-18 18:13:30 +0300

    Time: 84 ms (74.28%), Space: 43.7 MB (54.06%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
@@ -12,40 +12,47 @@
  * @param {Node} head
  * @return {Node}
  */
+
 // var flatten = function(head) {
-//     if (!head) return head
-//     let ans = new Node(0, null, null, null)
-//     let writer = ans
-//     const helper = (head) => {
-//         for (let p = head; p !== null; p = p.next) {
-//             writer.next = new Node(p.val, writer, null, null)
-//             writer = writer.next
-//             if (p.child) helper(p.child)
+//     let ptr = head
+//     const stack = []
+    
+//     while (ptr) {
+//         if (ptr.child) {
+//             if (ptr.next) stack.push(ptr.next)
+//             ptr.next = ptr.child
+//             ptr.next.prev = ptr
+//             ptr.child = null
 //         }
+//         else {
+//             if (!ptr.next && stack.length) {
+//                 ptr.next = stack.pop()
+//                 if(ptr.next) ptr.next.prev = ptr
+//             }
+//         }
+            
+//         ptr = ptr.next
 //     }
-//     helper(head)
-//     ans.next.prev = null
-//     return ans.next
+//     return head
 // };
 
 var flatten = function(head) {
     let ptr = head
-    const stack = []
-    
+ 
     while (ptr) {
         if (ptr.child) {
-            if (ptr.next) stack.push(ptr.next)
+            const next = ptr.next
+            
             ptr.next = ptr.child
             ptr.next.prev = ptr
+            
+            let c = ptr.child
+            while (c.next) {c = c.next}
+            c.next = next
+            if (next) next.prev = c
+            
             ptr.child = null
         }
-        else {
-            if (!ptr.next && stack.length) {
-                ptr.next = stack.pop()
-                if(ptr.next) ptr.next.prev = ptr
-            }
-        }
-            
         ptr = ptr.next
     }
     return head
