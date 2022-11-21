60f61e0b0629ea8c0f2996db790339e1c4ec3588
Dmitry <44382546+zeeeeby@users.noreply.github.com>
2022-11-21 22:49:55 +0300

    Time: 70 ms (88.11%), Space: 43.4 MB (83.40%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
@@ -13,47 +13,24 @@
  * @return {Node}
  */
 
-// var flatten = function(head) {
-//     let ptr = head
-//     const stack = []
-    
-//     while (ptr) {
-//         if (ptr.child) {
-//             if (ptr.next) stack.push(ptr.next)
-//             ptr.next = ptr.child
-//             ptr.next.prev = ptr
-//             ptr.child = null
-//         }
-//         else {
-//             if (!ptr.next && stack.length) {
-//                 ptr.next = stack.pop()
-//                 if(ptr.next) ptr.next.prev = ptr
-//             }
-//         }
-            
-//         ptr = ptr.next
-//     }
-//     return head
-// };
-
 var flatten = function(head) {
-    let ptr = head
- 
-    while (ptr) {
-        if (ptr.child) {
-            const next = ptr.next
-            
-            ptr.next = ptr.child
-            ptr.next.prev = ptr
-            
-            let c = ptr.child
-            while (c.next) {c = c.next}
-            c.next = next
-            if (next) next.prev = c
-            
-            ptr.child = null
-        }
-        ptr = ptr.next
-    }
-    return head
+   let ptr = head
+   while (ptr) {
+       if (ptr.child) {
+           const next = ptr.next
+           
+           ptr.next = ptr.child
+           ptr.next.prev = ptr
+           let c = ptr.child
+           while (c.next) c = c.next
+           
+           c.next = next
+           if (next) next.prev = c
+           
+           ptr.child = null
+       }
+       
+       ptr = ptr.next
+   }
+   return head
 };
